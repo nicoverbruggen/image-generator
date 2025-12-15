381aa5685c6726112ca74f54a083ac4553c3ec45
Nico Verbruggen <mail@nicoverbruggen.be>
2025-12-15 14:55:59 +0100

    Add validation for server

--- server/index.php
+++ server/index.php
@@ -13,15 +13,30 @@ if (array_key_exists('size', $_GET)) {
 }
 
 if (array_key_exists('text_color', $_GET)) {
-    $textColor = '#' . $_GET['text_color'];
+    // Sanitize: only allow valid hex characters
+    $color = preg_replace('/[^a-fA-F0-9]/', '', $_GET['text_color']);
+    if ($color !== '') {
+        $textColor = '#' . $color;
+    }
 }
 
 if (array_key_exists('background_color', $_GET)) {
-    $backgroundColor = '#' . $_GET['background_color'];
+    // Sanitize: only allow valid hex characters
+    $color = preg_replace('/[^a-fA-F0-9]/', '', $_GET['background_color']);
+    if ($color !== '') {
+        $backgroundColor = '#' . $color;
+    }
 }
 
-return (new ImageGenerator(
-    targetSize: $size,
-    textColorHex: $textColor,
-    backgroundColorHex: $backgroundColor,
-))->generate();
+try {
+    return (new ImageGenerator(
+        targetSize: $size,
+        textColorHex: $textColor,
+        backgroundColorHex: $backgroundColor,
+    ))->generate();
+} catch (\InvalidArgumentException $e) {
+    http_response_code(400);
+    header('Content-Type: text/plain');
+    echo 'Bad Request: ' . $e->getMessage();
+    exit;
+}
